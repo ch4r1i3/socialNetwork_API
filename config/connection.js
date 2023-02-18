851961c4ed5f7b5e7eac234fc70bc7fda2a84600
ch4r1i3 <111701536+ch4r1i3@users.noreply.github.com>
2023-02-18 14:43:59 -0500

    correcting connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const { connect, connection } = require('mongoose');
 
 const connectionString =
-    process.env.MONGODB_URI || 'mongodb://localhost:27017/socialNetwork_DB';
+    process.env.MONGODB_URI || 'mongodb://localhost:27017/SocialNetwork_DB';
 
 connect(connectionString, {
     useNewUrlParser: true,
